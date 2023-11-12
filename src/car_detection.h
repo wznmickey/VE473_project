3169eb7e2f528b1af0021bb13a83a00047c0995f
zjc_he <zjc_he@sjtu.edu.cn>
2023-11-12 17:41:04 +0800

    feat: baseline 11/12

---
+++ src/car_detection.h
@@ -0,0 +1,33 @@
+#ifndef CAR_DETECTION_H
+#define CAR_DETECTION_H
+
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+#include <opencv2/dnn.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include "car.h"
+
+using namespace cv;
+using namespace dnn;
+using namespace std;
+
+class Car_Detection
+{
+    public:
+        Car_Detection();
+        ~Car_Detection(){};
+        void callNetworks(Mat & frame);
+        vector<String> getOutputsNames(const Net& net);
+    private:
+        void findCars(Mat& frame, const vector<Mat>& outs, float confThreshold = 0.3);
+        Net net;
+};
+
+
+
+
+
+#endif
